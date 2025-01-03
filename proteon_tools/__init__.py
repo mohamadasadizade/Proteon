1f9221af0a76439d40f52df8d9d906b77c41b93b
Mohammad Assadizadeh <99049542+mohamadasadizade@users.noreply.github.com>
2025-01-03 04:49:04 +0330

    Update __init__.py

--- proteon_tools/__init__.py
+++ proteon_tools/__init__.py
@@ -1,8 +1,5 @@
-from .proteon_tools.analysis import *
-from .proteon_tools.io import *
-from .proteon_tools.manipulator import *
+from .io import *
+from .analysis import *
+from .manipulator import *
 
-__version__ = '1.0'
-
-print("Proteon package initialized")
 
